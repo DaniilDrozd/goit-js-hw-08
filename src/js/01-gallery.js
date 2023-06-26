b6f90659bc2e2d581cdcedb02fb309ceb2efa476
DaniilDrozd <124815236+DaniilDrozd@users.noreply.github.com>
2023-06-26 22:42:19 +0300

    have problem with import

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
-// Add imports above this line
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryMarkUp = document.querySelector('.gallery');
+
+
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryMarkUp.insertAdjacentHTML("beforeend",imagesMarkup);
+
+function createItemsMarkup(items){
+return items.map(({preview, original, description}) => {
+    return  ` <li class="gallery__item">
+    <a class="gallery__link" href="${preview}">
+    <img src="${preview}" class="gallery__image" data-source="${original}" alt="${description}">
+    </a>
+  </li>`;
+})
+.join("");
+}
+galleryMarkUp.addEventListener("click", onContainerClick);
+var lightbox = new SimpleLightbox('.gallery a',{
+    captionsData : 'alt',
+    captionsDelay:250,
+});
+
