537eb48047480798a4336fb5c81d4a7aa8b03ffb
DaniilDrozd <124815236+DaniilDrozd@users.noreply.github.com>
2023-06-27 16:19:36 +0300

    ex 1 end

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,7 +7,6 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
-
 const galleryMarkUp = document.querySelector('.gallery');
 
 
@@ -16,7 +15,7 @@ galleryMarkUp.insertAdjacentHTML("beforeend",imagesMarkup);
 
 function createItemsMarkup(items){
 return items.map(({preview, original, description}) => {
-    return  ` <li class="gallery__item">
+    return  ` 
     <a class="gallery__link" href="${preview}">
     <img src="${preview}" class="gallery__image" data-source="${original}" alt="${description}">
     </a>
@@ -24,7 +23,20 @@ return items.map(({preview, original, description}) => {
 })
 .join("");
 }
+
+const onContainerClick = (e) => {
+e.preventDefault();
+if (e.target.classList.contains("gallery")) return;
+const source = e.target.dataset.source;
+const modal = basicLightbox.create(
+    `<img src="${source}" width="800" height="600">`      
+);
+modal.show();
+};
+
+
 galleryMarkUp.addEventListener("click", onContainerClick);
+
 var lightbox = new SimpleLightbox('.gallery a',{
     captionsData : 'alt',
     captionsDelay:250,
