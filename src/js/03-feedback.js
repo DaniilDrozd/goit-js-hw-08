d1e0431b66ed3d1ecd6b79bfc0e119fab0fa6374
DaniilDrozd <124815236+DaniilDrozd@users.noreply.github.com>
2023-06-28 17:35:31 +0300

    end

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
 
+const FEEDBACK_F_ST = 'feedback-from-state';
+const feedbackForm = document.querySelector('.feedback-form');
+
+function onInput() {};
+
+
+feedbackForm.addEventListener('input', throttle(onInput, 500));
+feedbackForm.addEventListener('submit', onFormSubmit);
+
+function handleReloadPage(){
+const formData = JSON.parse(localStorage.getItem(FEEDBACK_F_ST)) || {};
+
+
+const { email, message } = feedbackForm.elements;
+
+
+email.value = formData.email || '';
+message.value = formData.message || '';
+}
+
+
+handleReloadPage();
+
+function onFormSubmit(event){
+    event.preventDefault();
+
+    const emailValue = feedbackForm.elements.email.value;
+    const messageValue = feedbackForm.elements.message.value;
+
+    if (emailValue === ''  || messageValue === ''){
+        alert('Please fill in all the fields!');
+        return;
+      }
+
+
+      const formData = {
+        email: emailValue,
+        message: messageValue
+      };
+
+
+    console.log(formData);
+
+    localStorage.setItem(FEEDBACK_F_ST, JSON.stringify(formData));
+
+feedbackForm.reset();
+}
